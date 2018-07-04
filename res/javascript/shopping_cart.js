0b836884097f5a57dd8ff0e61515ac416c2069f7
Vincent Harvey <vharv008@uottawa.ca>
2018-07-04 13:08:37 -0400

    Shopping Cart DONE.

--- res/javascript/shopping_cart.js
+++ res/javascript/shopping_cart.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
   //alert(books[booksInCart[0]].name);
   for (i = 0; i < 3; i++) {
     console.log("yep");
-    $("#items").append("<div class='item'>\n" +
-                "<div class='buttons'>\n" +
+    $(".shopping-cart").append("<div class='item'>\n" +
+                "<div class='buttonsSC'>\n" +
                   "<span class='delete-btn'></span>\n" +
                   "<span class='like-btn'></span>\n" +
                 "</div>\n" +
                 "<div class='image'>\n" +
-                  "<img src='" + books[booksInCart[i]].url + "' alt='"+ books[booksInCart[i]].category +"' />\n" +
+                  "<img src='" + books[booksInCart[i]].url + "' alt='"+ books[booksInCart[i]].category +"' heigth='100px' width='66.7px' />\n" +
                 "</div>\n" +
                 "<div class='description'>\n" +
                   "<span>"+ books[booksInCart[i]].name +"</span>" +
@@ -20,17 +20,60 @@ $(document).ready(function () {
                 "</div>\n" +
                 "<div class='quantity'>\n" +
                   "<button class='plus-btn' type='button' name='button'>\n" +
-                    "<i class='far fa-minus-square'></i>\n" +
+                    "<img src='res/images/plus.png' alt='' />\n" +
                   "</button>\n" +
-                  "<input type='text' name='Quantity' value='"+ quantity[i] + "'disabled>\n" +
+                  "<input id='"+ booksInCart[i] +"' type='text' name='Quantity' value='"+ quantity[i] + "'>\n" +
                   "<button class='minus-btn' type='button' name='button'>\n" +
-                    "<i class='far fa-plus-square'></i>\n" +
+                    "<img src='res/images/minus.png' alt='' />\n" +
                   "</button>\n" +
                 "</div>\n" +
-                "<div class='total-price'>" + (quantity[i] * books[booksInCart[i]].price).toFixed(2) + "$</div>\n" +
+                "<div id='"+ booksInCart[i] +"' class='total-price'>" + (quantity[i] * books[booksInCart[i]].price).toFixed(2) + "$</div>\n" +
               "</div>\n");
   }
+
+  $('.minus-btn').click(function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var id = $input.attr("id");
+    var value = parseInt($input.val());
+
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+
+    $input.val(value);
+    changeTotal(value, id);
+    });
+
+  $('.plus-btn').click(function(e) {
+      e.preventDefault();
+      var $this = $(this);
+      var $input = $this.closest('div').find('input');
+      var id = $input.attr("id");
+      var value = parseInt($input.val());
+
+      if (value < 100) {
+          value = value + 1;
+      } else {
+          value = 100;
+      }
+
+      $input.val(value);
+      changeTotal(value, id);
+  });
+
 });
+
+function changeTotal(value, id) {
+  $("#"+ id +".total-price").html((value * getPrice(id)).toFixed(2) + "$");
+};
+
+function getPrice(bookID) {
+  return (books[bookID].price);
+}
 
 function generateBooks() {
   var genbooks = [];
